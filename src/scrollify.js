86391a8d627a5872896b4e069e8ccb2d02331456
wes hatch <wes.hatch@gmail.com>
2016-06-14 23:43:55 -0400

    minor thoughts for fixifying

--- src/scrollify.js
+++ src/scrollify.js
@@ -22,10 +22,6 @@ for (let i in transforms) {
 }
 
 
-// Math.easeInOutQuad = function (t, b, c, d) { t /= d/2; if (t < 1) { return c/2*t*t + b; } t--; return -c/2 * (t*(t-2) - 1) + b; };
-// Math.easeOutCubic = function (t, b, c, d) { t /= d; t--; return c*(t*t*t + 1) + b; };
-
-
 /**
  * A list of some default "transformations" that may be applied
  * NOTE: don't use arrow fn's here as they proxy "this"
@@ -39,10 +35,10 @@ var effectList = {
 	parallax(opts) {
 		let offset = 0;
 
-		if (opts.speed !== undefined) {									// check speed first
-		 	offset = this.absolute * opts.speed;
-		} else {																				// fallback to range
-		 	offset = this.percent * (opts.range || 0);		// default is "0", no effect
+		if (opts.speed !== undefined) {                 // check speed first
+			offset = this.absolute * opts.speed;
+		} else {                                        // fallback to range
+			offset = this.percent * (opts.range || 0);    // default is "0", no effect
 		}
 
 		this.el.style[transform] = 'translate(0, '+ offset +'px)';
@@ -54,33 +50,33 @@ var effectList = {
 	 * ... while this works, it is pretty ugly and candidate for improvement
 	 */
 	// pin(opts) {
-	// 	let waypoints = Object.keys(opts);
-	// 	let percent = this.percent * 100;
+	//  let waypoints = Object.keys(opts);
+	//  let percent = this.percent * 100;
 
-	// 	waypoints.forEach(where => {
-	// 		if (percent < parseInt(where)) {
+	//  waypoints.forEach(where => {
+	//    if (percent < parseInt(where)) {
 
-	// 			let distance = opts[where];
-	// 			let absolute = this.absolute;
-	// 			var current;
+	//      let distance = opts[where];
+	//      let absolute = this.absolute;
+	//      var current;
 
-	// 			if (this.current) {
-	// 				current = this.current;
-	// 			} else {
-	// 				current = absolute;
-	// 				this.current = current;
-	// 			}
+	//      if (this.current) {
+	//        current = this.current;
+	//      } else {
+	//        current = absolute;
+	//        this.current = current;
+	//      }
 
-	// 			let end = current + distance;	// (this assumes current will be "frozen" and unchanged while pinned)
-	// 			let offset = absolute - current;
+	//      let end = current + distance; // (this assumes current will be "frozen" and unchanged while pinned)
+	//      let offset = absolute - current;
 
-	// 			if (absolute < end) {
-	// 				this.el.style[transform] = 'translate(0, '+ offset +'px)';
-	// 			}
-	// 		} else {
-	// 			// this.el.style[transform] = 'translate(0, 0)';
-	// 		}
-	// 	});
+	//      if (absolute < end) {
+	//        this.el.style[transform] = 'translate(0, '+ offset +'px)';
+	//      }
+	//    } else {
+	//      // this.el.style[transform] = 'translate(0, 0)';
+	//    }
+	//  });
 	// },
 
 
@@ -110,7 +106,25 @@ var effectList = {
  */
 export default class Scrollify {
 
-	constructor(element) {
+	/**
+	 * params: any TWO of: start / stop / duration.
+	 *         start: a percentage of the viewport (eg. 0.5) OR a reference element's position (eg ['#toggle', 0.3] )
+	 *         stop: a percentage of the viewport OR a reference element's position
+	 *         duration: the duration in pixels
+	 *
+	 *         default is 0 - 100% (making duration the window height + element height)
+	 *
+	 *         examples:
+	 *          { start: 0, stop: 0.5 }
+	 *          { start: 0.1, duration: '400px' }
+	 *          { duration: 100px, stop: 1.0 }
+	 *          { start: ['#toggle', 0.3], stop: ['#toggle', 0.5] }
+	 *          { start: ['#toggle', 0.3], duration: '300px' }
+	 *
+	 *         easing...? start, to, from, duration
+	 *
+	 */
+	constructor(element, scene={}) {
 		let elements = (element instanceof HTMLElement) ? [element] : document.querySelectorAll(element);
 		if ( !elements.length || !transform ) { return false; }
 
@@ -133,7 +147,7 @@ export default class Scrollify {
 	 */
 	initialize() {
 		this.elements.map((data) => {
-			let BCR = data.el.getBoundingClientRect();	// TODO use offsetTop
+			let BCR = data.el.getBoundingClientRect();  // TODO use offsetTop
 
 			data.initial = {
 				top:  BCR.top + window.scrollY,
@@ -141,10 +155,23 @@ export default class Scrollify {
 				height: BCR.height
 			};
 
+			// scene:
+			let start, duration;
+			if (duration && !start) { start = (end * window.innerHeight - duration) / window.innerHeight; }
+			if (start && Array.isArray(start)) {
+				BCR = document.querySelector(start[0]).getBoundingClientRect().top; // TODO use offsetTop
+				start = start[1]
+			}
+
+			//
+			data.start = (start * window.innerHeight) + BCR.top + window.scrollY;
+			data.duration = duration ? duration : (stop-start) * window.innerHeight;
+			//
+
 			this.calculate(data);
 			return data;
 		});
-  }
+	}
 
 	/**
 	 *
@@ -159,7 +186,7 @@ export default class Scrollify {
 	 */
 	useEffect(name, options) {
 		let curry = (fn, options) => {
-			return function() {				// NOTE: don't use => function here as we do NOT want to bind "this"
+			return function() {       // NOTE: don't use => function here as we do NOT want to bind "this"
 				fn.call(this, options);
 			}
 		}
@@ -201,23 +228,26 @@ export default class Scrollify {
 	calculate(data) {
 		let height = window.innerHeight;
 		let start = data.initial.top - this.scroll;
-		let end = data.initial.bottom - this.scroll;
+		// let end = data.initial.bottom - this.scroll;
 		let h = data.initial.height;
 		let percent;
 
 		// dont do nuthin until this here thing is within range (ie. top edge peeks out from the bottom of the screen)
-		// if (height < start || 0 > end) { return; }		// note: this wont work as the position of each element changes at different rates.
+		// if (height < start || 0 > end) { return; }   // note: this wont work as the position of each element changes at different rates.
 
-		if (height < data.el.getBoundingClientRect().top || 0 > data.el.getBoundingClientRect().bottom) { return; }	// use *actual* position data
+		if (height < data.el.getBoundingClientRect().top || 0 > data.el.getBoundingClientRect().bottom) { return; } // use *actual* position data
 
 		// Calculate how far across the screen the element is. "1" is when the top edge of the element first peeks out
 		// from the bottom of the viewport, and "0" is when the bottom edge disappears beyond the top of the viewport:
-		// percent = Math.min(1, start / height);			// 1 --> 0
-		percent = (start+h) / (height+h);					// 1 --> 0
+		// percent = Math.min(1, start / height);     // 1 --> 0
+		percent = (start+h) / (height+h);         // 1 --> 0
 
 		// update data Object
-		data.percent = percent;						// [TODO] should this be 0 -> 100 ...?
+		data.percent = percent;           // [TODO] should this be 0 -> 100 ...?
 		data.absolute = height - start;
+
+																// start      to  from  end
+		// let easing = easeInOutQuad(data.start, 100, 0, data.start+data.duration);
 
 		// cycle through any registered transformations
 		this.effects.forEach((effect) => { effect.call(data) });
