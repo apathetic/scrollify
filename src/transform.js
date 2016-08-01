08dab67c7e5c342c3ae9930f41ea8d0866eca7fc
wes hatch <wes.hatch@gmail.com>
2016-08-01 12:53:50 -0400

    updating transform, doesnt need DOM ready

--- src/transform.js
+++ src/transform.js
@@ -3,8 +3,10 @@
  * @type {Boolean}
  */
 
-let transform = ['transform', 'webkitTransform', 'MozTransform', 'OTransform', 'msTransform'].find((t) => {
-  return (document.body.style[t] !== undefined);
+const dummy = document.createElement('div');
+const transform = ['transform', 'webkitTransform', 'MozTransform', 'OTransform', 'msTransform'].find((t) => {
+  // return (document.body.style[t] !== undefined);   // if DOM is not yet ready, let's do:
+  return (dummy.style[t] !== undefined);
 });
 
 export default transform;
